b59f30cafc8d4c2dd7a7f2d62935040bec71726b
energypatrikhu <energypatrikhu@proton.me>
2025-12-28 21:49:06 +0100

    feat: add build configuration with manual chunking for node_modules

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,4 +4,16 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
+  build: {
+    target: "es2016",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        },
+      },
+    },
+  },
 });
